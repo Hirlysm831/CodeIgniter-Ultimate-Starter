c4659999c7861cf076bc5efe7068e1bfdd739d46
Hirlysm831 <hirlysm831@gmail.com>
2018-07-01 21:44:43 +0800

    doc comment structure at index.php

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
+
+
+//**************** Start COMMENTING STRUCTURE ******************//
+
+//======================================================================
+// CATEGORY LARGE FONT
+//======================================================================
+
+//-----------------------------------------------------
+// Sub-Category Smaller Font
+//-----------------------------------------------------
+
+/* Title Here Notice the First Letters are Capitalized */
+
+# Option 1
+# Option 2
+# Option 3
+
+/*
+* This is a detailed explanation
+* of something that should require
+* several paragraphs of information.
+*/
+
+// This is a single line quote.
+//****************End COMMENTING STRUCTURE******************//
+
 /**
  * CodeIgniter
  *
