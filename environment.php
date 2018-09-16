f34e38dab64e384b2b6121ed828e537b42feae8d
rmfitcocoy <francisco.abayon@rafi.org.ph>
2018-09-17 01:25:07 +0800

    enh http request in the environment.php file
    cron job for editing the documentation and alignment of spaces

--- environment.php
+++ environment.php
@@ -14,39 +14,48 @@
  * @url				https://phpdelusions.net/articles/empty
  *
  ***************************************************************************/
-
-/****************
+ 
+ //=========================================================================
+ // Configuration Setup 
+ //=========================================================================
+/***************************************************************************
+ * 
+ * @description  	This can be set to anything, but default usage is:
+ * 					- development
+ *  				- testing
+ *  				- production
  *
- * This can be set to anything, but default usage is:
- *  - development
- *  - testing
- *  - production
- *     
- *     
- * It's in two places - let's be smart
- * Our goal is to send:
- *   - php index.php cron daily_tasks important_job 123 --env production
- *	 - and have it run http://mysite.com/cron/daily_tasks/important_job/123 
- *	   using the production environment
- *
- ****************/
-define('DEFAULT_ENV', 'development');
+ * @category		instantation 
+ * @package	    	environment_pack
+ * @todo	    	- Optimize the domain name with its corresponding data
+ * @author  		Francisco Abayon 
+ * @copyright		September 17, 2018
+ * @version  		0.1.0
+ * 						It's in two places - let's be smart
+ * 						Our goal is to send:
+ *   						- php index.php cron daily_tasks important_job 123 --env production
+ *							- and have it run http://mysite.com/cron/daily_tasks/important_job/123 
+ *	  						  using the production environment
+ * 					
+ ***************************************************************************/
+define('DEFAULT_ENV', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 $hostname_development 	= array('localhost','http://test.ultimatemvc.com:8080');
 $hostname_testing 		= array('http://test.ultimatemvc.com:8080','http://dikol/test');
 $hostname_production	= array('https://www.yoursite.tld','http://www.yoursite.tld');
 
-/****************
+/***************************************************************************
  *
  * Having conditional statement for not being access via cli is set to by boolean and check
  * if being access via cli it well automatically change the environment on it
  *
  * @todo		Create a case expression for determining the CLI or web in HTTP_HOST
- * @var			Boolean						Environement act ast the trigger point
+ * @var			Boolean						environment act ast the trigger point
  *
- ****************/
-
+ ***************************************************************************/
+ 
 //--> Start of cli code here and detects if it is a command line request
-if ((php_sapi_name() == 'cli') or defined('STDIN')){
+if ((php_sapi_name() == 'cli') or defined('STDIN'))
+{
 	$args = array_slice($_SERVER['argv'], 1);
 	$args ? implode('/', $args) : '';
 	
@@ -61,18 +70,23 @@ if ((php_sapi_name() == 'cli') or defined('STDIN')){
 	}  
   	define('ENVIRONMENT', $environment);
 	//--> End of CLI code here
-} else{
-	
-	//--> Start of web request access
-/****************
+} 
+
+else
+{
+//--> Start of web request access
+/***************************************************************************
  *
  * Not safe using on $_SERVER['HTTP_HOST'] but just be careful...
+ * @abstract
+ * @access
+ * @see		https://stackoverflow.com/questions/10350602/how-safe-is-serverhttp-host
+ * @see		https://stackoverflow.com/questions/6474783/which-server-variables-are-safe
+ * @see 	https://stackoverflow.com/questions/2297403/what-is-the-difference-between-http-host-and-server-name-in-php
  *
- * @url				https://stackoverflow.com/questions/10350602/how-safe-is-serverhttp-host
- * @url				https://stackoverflow.com/questions/6474783/which-server-variables-are-safe
- * @url				https://stackoverflow.com/questions/2297403/what-is-the-difference-between-http-host-and-server-name-in-php
- *
- ****************/
+ ***************************************************************************/
+ 
+// Get the requestor host either bird who already suicide any
 list($realHost,)=explode(':',$_SERVER['HTTP_HOST']);
 $domain = strtolower($realHost);
 	
@@ -96,5 +110,9 @@ $domain = strtolower($realHost);
 	}	
 	//--> End of web access of the system here
 }
-/* End of file environement.php */
-/* Location: ./environement.php */
+/**************************************************************************** 
+ *	@abstract
+ * 	@access
+ * 	End of file environment.php 
+ * 	Location: ./environment.php 
+ ****************************************************************************//
